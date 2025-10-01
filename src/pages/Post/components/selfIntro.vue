9d2de00721f3ac90e2fd5423c2cc0575d18b62c4
SC1314520 <g0710126@hchs.hc.edu.tw>
2025-10-01 20:13:44 +0800

    修改 selfIntro 的 overflow 模式

--- src/pages/Post/components/selfIntro.vue
+++ src/pages/Post/components/selfIntro.vue
@@ -6,26 +6,28 @@
         <div class="name_chinese">林英豪</div>
         <div class="name_Englist">YING-HAO, LIN</div>
     </div>
-    <div class="part">
-        <div class="part_title">學歷</div>
-        <div class="part_detail">國立清華大學半導體研究學院 <span style="font-weight: bolder;">碩士*</span></div>
-        <div class="part_detail">國立清華大學工程與系統科學系 <span style="font-weight: bolder;">學士</span></div>
-    </div>
-    <div class="part">
-        <div class="part_title">研究領域</div>
-        <div class="part_detail">前瞻半導體二維材料元件設計</div>
-    </div>
-    <div class="part">
-        <div class="part_title">工作經驗</div>
-        <div class="work_name">國家高速網路與計算中心 <div class="work_time">202507-202509</div></div>
-        <div class="work_title">大數據服務研發工程師（實習）</div>
-        <div class="work_name">國家高速網路與計算中心 <div class="work_time">202407-202507</div></div>
-        <div class="work_title">PINNs 前端開發工程師（實習）</div>
-    </div>
-    <div class="part">
-        <div class="part_title">自我簡介</div>
-        <div class="part_personal_intro">我平時熱衷於鑽研電子電路與人工智慧應用，也喜歡透過專案實作培養解決問題的能力。在探索新知的過程中，我深刻體會到<span style="font-weight: bolder;">「誠信」</span>的重要性，因為唯有誠信才能讓團隊彼此信任，讓創新成果被業界與客戶認可。我希望未來能以專業結合誠懇態度，在半導體產業持續貢獻與成長。</div>
-    </div>
+   <div class="detail">
+        <div class="part">
+            <div class="part_title">學歷</div>
+            <div class="part_detail">國立清華大學半導體研究學院 <span style="font-weight: bolder;">碩士*</span></div>
+            <div class="part_detail">國立清華大學工程與系統科學系 <span style="font-weight: bolder;">學士</span></div>
+        </div>
+        <div class="part">
+            <div class="part_title">研究領域</div>
+            <div class="part_detail">前瞻半導體二維材料元件設計</div>
+        </div>
+        <div class="part">
+            <div class="part_title">工作經驗</div>
+            <div class="work_name">國家高速網路與計算中心 <div class="work_time">202507-202509</div></div>
+            <div class="work_title">大數據服務研發工程師（實習）</div>
+            <div class="work_name">國家高速網路與計算中心 <div class="work_time">202407-202507</div></div>
+            <div class="work_title">PINNs 前端開發工程師（實習）</div>
+        </div>
+        <div class="part">
+            <div class="part_title">自我簡介</div>
+            <div class="part_personal_intro">我平時熱衷於鑽研電子電路與人工智慧應用，也喜歡透過專案實作培養解決問題的能力。在探索新知的過程中，我深刻體會到<span style="font-weight: bolder;">「誠信」</span>的重要性，因為唯有誠信才能讓團隊彼此信任，讓創新成果被業界與客戶認可。我希望未來能以專業結合誠懇態度，在半導體產業持續貢獻與成長。</div>
+        </div>
+   </div>
   </div>
 </template>
 
@@ -44,8 +46,7 @@ export default {
         transition: all 1s ease;
         box-sizing: border-box;
         padding-top: 20px;
-        padding-bottom: 20px;
-        overflow-y: scroll;
+        padding-bottom: 15px;
     }
     .box:hover{
         cursor: pointer;
@@ -64,6 +65,14 @@ export default {
         background-position: center;
         background-size: contain;
         background-repeat: no-repeat;
+    }
+    .detail{
+        width: 100%;
+        min-height: 395px;
+        height: calc(100vh - 390px);
+        box-sizing: border-box;
+        overflow-y: scroll;
+        padding-bottom: 20px;
     }
     .name{
         width: 100%;
