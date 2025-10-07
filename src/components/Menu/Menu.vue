1741ecb5ce5da4594f0a72a8f3b70d58d4632ee2
SC1314520 <g0710126@hchs.hc.edu.tw>
2025-10-07 20:17:14 +0800

    version 1.0.0.7

--- src/components/Menu/Menu.vue
+++ src/components/Menu/Menu.vue
@@ -29,7 +29,7 @@
     </div>
     <div v-if="isLogin" :class="{list:true}" @click="logout()">登出系統</div>
     <div class="version_info">
-        <div>General Version 1.0.0.6</div>
+        <div>General Version 1.0.0.7</div>
     </div>
     <div class="footer" @click="goTo('/')"><img class="footer_img" src="img/horizontal_logo_white.png" alt="" > </div>
   </div>
